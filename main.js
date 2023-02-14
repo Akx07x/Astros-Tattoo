c2b690fe3a7151949ec7a78b1f8911f782936852
Agustín <117188465+Akx07x@users.noreply.github.com>
2023-02-14 11:29:58 +0100

    Astros Tattoo

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 document.querySelector('.btn-menu').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
+
+ScrollReveal().reveal('.container-hero');
+ScrollReveal().reveal('.tittle-section', { delay: 500});
+ScrollReveal().reveal('.container-realismo', { delay: 500});
+ScrollReveal().reveal('.container-hero-2', { delay: 500});
+ScrollReveal().reveal('.container-geometricos', { delay: 500});
+ScrollReveal().reveal('.container-hero-3', { delay: 500});
+ScrollReveal().reveal('.container-tradicionales', { delay: 500});
